8484d83c99202dd39d51c7b538882cd1e24a8e0f
kcrong <iam@devkcr.org>
2018-05-09 19:00:17 +0900

    Add ropsten network at truffle.js

--- truffle.js
+++ truffle.js
@@ -14,6 +14,12 @@ module.exports = {
             network_id: "15",
             gas: 4692388,
             gasPrice: 100000 // Specified in Wei
+        },
+        ropsten: {
+            host:"localhost",
+            port:8545,
+            network_id: 3,
+            from: "0x8F8d1bc97E8939e3932BfBeb923A1B2B972D5A9A",
         }
     },
     rpc: {
